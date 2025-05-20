d49c97ae59b5b2d0b5a85db9b7d0128a38f721c6
landanfp <159138257+landanfp@users.noreply.github.com>
2025-05-20 15:52:48 +0330

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,9 @@
+class Translation(object):
+    DOWNLOAD_START = "در حال شروع دانلود..."
+    UPLOAD_START = "در حال شروع آپلود..."
+    TECH_VJ_CUSTOM_CAPTION_UL_FILE = "فایل توسط {mention} آپلود شد."
+    TECH_VJ_SLOW_URL_DECED = "لینک شما خیلی کُند است یا نامعتبر. لطفاً یک لینک معتبر و سریع‌تر امتحان کنید."
+    TECH_VJ_RCHD_TG_API_LIMIT = "متاسفم! این فایل از محدودیت حجم فایل تلگرام (2GB) بیشتر است."
+    TECH_VJ_NO_VOID_FORMAT_FOUND = "خطا: {reason}. هیچ فرمت قابل دانلودی یافت نشد."
+    TECH_VJ_AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "دانلود در {} ثانیه کامل شد.\nآپلود در {} ثانیه کامل شد."
+    TECH_VJ_UPLOAD_START = "در حال آپلود..."
